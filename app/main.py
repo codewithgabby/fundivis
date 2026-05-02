82e878bcee77b6ed728b5e8084322de94cde6cf8
Gabriel Johnson <johnfem4real@gmail.com>
2026-05-02 01:33:12 +0100

    fix main

--- app/main.py
+++ app/main.py
@@ -44,7 +44,7 @@ app.add_middleware(
     allow_headers=["Authorization", "Content-Type"],
 )
 
-# Include routers
+
 app.include_router(auth.router)
 app.include_router(income.router)
 app.include_router(expense.router)
